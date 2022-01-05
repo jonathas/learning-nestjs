68e65e660e58203e5c290002257624ec4e99ad8e
Jonathas Ribeiro <jonathas.ribeiro@cloudtalk.io>
2022-01-05 22:55:29 +0100

    feat: custom data serialization

---
+++ my-car-value/src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,25 @@
+import { NestInterceptor, ExecutionContext, CallHandler, UseInterceptors } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToInstance } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): unknown;
+}
+
+// Decorator
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> {
+    return next.handle().pipe(
+      map((data: any) => {
+        // Run something before the response is sent out
+        return plainToInstance(this.dto, data, { excludeExtraneousValues: true });
+      }),
+    );
+  }
+}
