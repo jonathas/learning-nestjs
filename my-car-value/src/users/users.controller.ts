ca787d61801a96d4b7352dcf8e06bfce031a2ae2
Jonathas Ribeiro <jonathas.ribeiro@cloudtalk.io>
2022-01-05 21:18:33 +0100

    feat: added remaining users endpoints

--- my-car-value/src/users/users.controller.ts
+++ my-car-value/src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -11,21 +12,23 @@ export class UsersController {
     return this.usersService.create(body.email, body.password);
   }
 
-  /*
-  findUser() {
-
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
   }
 
-  findAllUsers() {
-
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
   }
 
-  updateUser() {
-
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body);
   }
 
-  removeUser() {
-
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
-  */
 }
