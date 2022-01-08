f10a43af391c3a3b833d01e2512f4a127fe92677
Jonathas Ribeiro <jonathas.ribeiro@cloudtalk.io>
2022-01-08 17:53:25 +0100

    feat: db config for all envs and added first migration

--- my-car-value/src/app.module.ts
+++ my-car-value/src/app.module.ts
@@ -6,8 +6,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
 const cookieSession = require('cookie-session');
 
 @Module({
@@ -18,27 +16,18 @@ const cookieSession = require('cookie-session');
     }),
     UsersModule,
     ReportsModule,
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          entities: [User, Report], // repositories
-          synchronize: true // not to be used in production!
-        };
-      }
-    })
+    TypeOrmModule.forRoot()
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_PIPE, useValue: new ValidationPipe({ whitelist: true }) }]
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['adsaj1324']
+          keys: [this.configService.get('COOKIE_KEY')]
         })
       )
       .forRoutes('*');
