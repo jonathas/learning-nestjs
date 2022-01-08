9077d78e2ed0da2a22ea3fcb8d9021b88ffb25ec
Jonathas Ribeiro <jonathas.ribeiro@cloudtalk.io>
2022-01-08 15:24:37 +0100

    feat: added create report endpoint

---
+++ my-car-value/src/reports/dtos/report.dto.ts
@@ -0,0 +1,32 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  year: number;
+
+  @Expose()
+  lng: number;
+
+  @Expose()
+  lat: number;
+
+  @Expose()
+  make: string;
+
+  @Expose()
+  model: string;
+
+  @Expose()
+  mileage: number;
+
+  // Take the original report entity, look at its user property and get the id
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
+}
--- my-car-value/src/reports/reports.controller.ts
+++ my-car-value/src/reports/reports.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(@CurrentUser() user: User, @Body() body: CreateReportDto) {
+    return this.reportsService.create(body, user);
+  }
+}
